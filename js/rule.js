5bd7899c8d94ffe0187859f1680df0332b36a4f6
Ivan <delfatti.ivan@gmail.com>
2020-10-21 14:02:08 +0200

    Structure

---
+++ js/rule.js
@@ -0,0 +1,15 @@
+/***********************************
+ * CALCOLATORE COSTO DEL BIGLIETTO
+ **********************************/
+
+
+//  VARIABILI
+
+var nome = document.getElementById('nome');
+var km = document.getElementById('km');
+var eta = document.getElementById('eta');
+
+var offerta = document.getElementById('sconto');
+var carrozza = document.getElementById('carrozza');
+var codiceCp = document.getElementById('codice-cp');
+var costoTot = document.getElementById('costo-tot')
