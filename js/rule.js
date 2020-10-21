94d5a2d04957255751e0ab8a6792694a9283a761
Ivan <delfatti.ivan@gmail.com>
2020-10-21 14:33:09 +0200

    WIP

--- js/rule.js
+++ js/rule.js
@@ -2,14 +2,41 @@
  * CALCOLATORE COSTO DEL BIGLIETTO
  **********************************/
 
+genera.addEventListener('click', function() {
 
-//  VARIABILI
+    //  VARIABILI
 
-var nome = document.getElementById('nome');
-var km = document.getElementById('km');
-var eta = document.getElementById('eta');
+    // FORM
+    var nome = document.getElementById('nome').value;
+    var km = document.getElementById('km').value;
+    var eta = document.getElementById('eta').value;
 
-var offerta = document.getElementById('sconto');
-var carrozza = document.getElementById('carrozza');
-var codiceCp = document.getElementById('codice-cp');
-var costoTot = document.getElementById('costo-tot')
+    // BIGLIETTO
+    var nomeUtente = document.getElementById('nome-utente');
+    var offerta = document.getElementById('sconto');
+    var carrozza = document.getElementById('carrozza');
+    var codiceCp = document.getElementById('codice-cp');
+    var costoTot = document.getElementById('costo-tot')
+
+    // BUTTONS
+    var genera = document.getElementById('genera');
+    var reset = document.getElementById('reset');
+
+    // CONTROLLO
+
+    // CALCOLO CARROZZA E CODICER CP
+    carrozza.innerHTML = Math.floor( Math.random() *10 ) + 1;
+    codiceCp.innerHTML = Math.floor( Math.random() * ( 100000 - 90000 ) ) + 90000;
+
+    // CALCOLO COSTO BIGLIETTO
+
+    if ( eta === 'under18') {
+        alert('ciao')
+    } else if ( eta === 'over18' ) {
+        alert('nociao')
+    } else if ( eta === 'over65' ) {
+        alert('ciaone')
+    }
+
+    nomeUtente.innerHTML = nome;
+});
